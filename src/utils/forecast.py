a85cea17c85647597ae6d85c9fe45a019683bd36
Nikita <savvin.nikit@yandex.ru>
2025-07-03 13:42:00 +0300

    fix: timeout

--- src/utils/forecast.py
+++ src/utils/forecast.py
@@ -1,11 +1,28 @@
-import requests
 import pandas as pd
-import plotly.graph_objects as go
 
-base_url = "http://0.0.0.0:7078/backend/v1"
+
+df_init = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSgwB47qVFZcr1Aq--UWxZ6fDi9CGLZm-1i8QoMgfdaHUbV8EqSli3ayPxYYxD8kqfYYHD41uuNxbjZ/pub?gid=1952392108&single=true&output=csv")
+print(df_init.head(-5))
+
+
+time_column = 'Datetime'
+col_target = 'consumption'
+
+
+print(f"Последняя известная дата - {df_init[time_column].iloc[-1]}")
+
+
+forecast_horizon_time = '2018-01-03 23:45:00'
+
+
+import requests
+
+
+# base_url = "https://nikitasavvin2000-horizon-api-921e.twc1.net"
+base_url = "http://0.0.0.0:7079"
 
 def func_generate_forecast(df: pd.DataFrame, time_column: str, col_target: str, forecast_horizon_time: str):
-    url = f"{base_url}/generate_forecast"
+    url = f"{base_url}/forecast"
 
     df_records = df.to_dict(orient='records')
 
@@ -28,33 +45,20 @@ def func_generate_forecast(df: pd.DataFrame, time_column: str, col_target: str, 
         print(f"Ошибка при запросе: {e}")
         return None
 
-df_data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSgwB47qVFZcr1Aq--UWxZ6fDi9CGLZm-1i8QoMgfdaHUbV8EqSli3ayPxYYxD8kqfYYHD41uuNxbjZ/pub?gid=1952392108&single=true&output=csv")
 
-time_column = 'Datetime'
-col_target = 'consumption'
 
-forecast_horizon_time = '2018-01-07 23:45:00 '
 
 predict_dict = func_generate_forecast(
-    df=df_data,
+    df=df_init,
     time_column=time_column,
     col_target=col_target,
     forecast_horizon_time=forecast_horizon_time
 )
 
-predictions= predict_dict["map_data"]["data"]["predictions"]
+
+predictions= predict_dict["predictions"]
 df_predictions = pd.DataFrame(predictions)
 df_predictions = df_predictions.iloc[1:]
 
 print(df_predictions)
 
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    y=y_pred,
-    mode='lines',
-    name='Forecast',
-    line=dict(color='orange')
-))
-
-fig.show()
